8868bfe0cd4f870b144ae0ff346d1444d04c73f4
OTrading <otradingtv@gmail.com>
2021-01-23 15:50:50 -0500

    Initial commit

--- truffle-config.js
+++ truffle-config.js
@@ -6,8 +6,8 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
-    },
+      network_id: "*" // * = Match any network id
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
